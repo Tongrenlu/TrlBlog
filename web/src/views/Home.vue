2987f8e477f5e2341c6a38bec3e86566ff4d8650
Trl <526220143@qq.com>
2018-09-25 13:30:27 +0800

    前端增加了管理系统的登录页，技能树也随着代码的编写进行了更新

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/management">Management</router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home",
+  components: {}
+};
 </script>
